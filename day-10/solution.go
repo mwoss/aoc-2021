9045b434acf002ceafb08df522e64b6e866abfc1
Mateusz Wos <wos.mateusz16@gmail.com>
2021-12-26 22:42:39 +0100

    Add complete solution for day 10 - part 2

--- day-10/solution.go
+++ day-10/solution.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 )
 
 var leftToRightParentheses = map[string]string{
@@ -14,12 +15,20 @@ var leftToRightParentheses = map[string]string{
 	"<": ">",
 }
 
-var characterToValue = map[string]int{
+var corruptedCharacterToValue = map[string]int{
 	"":  0,
 	")": 3,
 	"]": 57,
 	"}": 1197,
 	">": 25137,
+}
+
+var missingCharacterToValue = map[string]int{
+	"":  0,
+	"(": 1,
+	"[": 2,
+	"{": 3,
+	"<": 4,
 }
 
 func GetPenaltyCorruptedLines() int {
@@ -36,13 +45,46 @@ func GetPenaltyCorruptedLines() int {
 
 	scanner := bufio.NewScanner(file)
 
-	var errorScore int
+	var penaltyScore int
 	for scanner.Scan() {
 		character := findFirstInvalidCharacter(scanner.Text())
-		errorScore += characterToValue[character]
+		penaltyScore += corruptedCharacterToValue[character]
 	}
 
-	return errorScore
+	return penaltyScore
+}
+
+func GetPenaltyIncompleteLines() int {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+
+	var penaltyScores []int
+	for scanner.Scan() {
+		linePenalty := 0
+		characters := findMissingCharacters(scanner.Text())
+
+		for i := range characters {
+			linePenalty *= 5
+			linePenalty += missingCharacterToValue[characters[len(characters)-1-i]]
+		}
+
+		if linePenalty != 0 {
+			penaltyScores = append(penaltyScores, linePenalty)
+		}
+	}
+
+	sort.Ints(penaltyScores)
+	return penaltyScores[len(penaltyScores)/2]
 }
 
 func findFirstInvalidCharacter(line string) string {
@@ -63,6 +105,25 @@ func findFirstInvalidCharacter(line string) string {
 	return ""
 }
 
+func findMissingCharacters(line string) []string {
+	stack := Stack{}
+	for _, char := range line {
+		par := string(char)
+		if _, ok := leftToRightParentheses[par]; ok {
+			stack.Push(par)
+		} else if len(stack) == 0 {
+			return []string{}
+		} else {
+			leftPar := stack.Pop()
+			if par != leftToRightParentheses[leftPar] {
+				return []string{}
+			}
+		}
+	}
+	return stack
+}
+
 func main() {
 	fmt.Println(GetPenaltyCorruptedLines())
+	fmt.Println(GetPenaltyIncompleteLines())
 }
