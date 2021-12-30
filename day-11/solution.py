a420a8bf6598c1e0519eecb58ab8e87eb3113e52
mwoss <wos.mateusz16@gmail.com>
2021-12-30 23:54:58 +0100

    Refactor (a bit) solution for day 11

--- day-11/solution.py
+++ day-11/solution.py
@@ -1,84 +1,73 @@
-import pprint
-import sys
+from copy import deepcopy
 from typing import List, Set, Tuple
 
-sys.setrecursionlimit(100000)
-
-
-def _propagate_energy(octo_cave: List[List[int]], already_flashed: Set[Tuple[int, int]], x: int, y: int):
-    neighbours = {
-        (x + 1, y + 1), (x - 1, y - 1), (x + 1, y - 1), (x - 1, y + 1), (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)
-    }
-    cave_size = len(octo_cave) - 1
-    flashes = 0
-
-    for neighbour in neighbours:
-        xn, yn = neighbour
-        if 0 > xn or xn > cave_size or 0 > yn or yn > cave_size:
-            continue
-        if (xn, yn) in already_flashed:
-            continue
-
-        octo_cave[xn][yn] += 1
-
-        if octo_cave[xn][yn] > 9:
-            flashes += 1
-            already_flashed.add((xn, yn))
-            octo_cave[xn][yn] = 0
-            flashes += _propagate_energy(octo_cave, already_flashed, xn, yn)
-
-    return flashes
+MIN_ADMISSIBLE_ENERGY = 0
+MAX_ADMISSIBLE_ENERGY = 9
 
 
 def count_octopuses_flashes(octo_cave: List[List[int]], n: int) -> int:
-    # simulate the flashes
-    cave_size = len(octo_cave)
-    flashes_count = 0
-    for day in range(n):
+    flashes_count, cave_size = 0, len(octo_cave)
+
+    for _ in range(n):
         already_flashed = set()
-        for i in range(cave_size):
-            for j in range(cave_size):
-                if (i, j) in already_flashed:
+        for x in range(cave_size):
+            for y in range(cave_size):
+                if (x, y) in already_flashed:
                     continue
 
-                octo_cave[i][j] += 1
-                if octo_cave[i][j] > 9:
-                    already_flashed.add((i, j))
-                    flashes_count += 1
-                    octo_cave[i][j] = 0
-                    flashes_count += _propagate_energy(octo_cave, already_flashed, i, j)
-
-        if (day + 1) % 10 == 0:
-            pprint.pprint(octo_cave)
-            print("---------------------")
+                flashes_count += _increment_octo_energy(x, y, octo_cave, already_flashed)
 
     return flashes_count
 
+
 def find_first_simultaneous_flash(octo_cave: List[List[int]], threshold: int) -> int:
-    # simulate the flashes
     cave_size = len(octo_cave)
 
     for day in range(threshold):
         already_flashed = set()
-        # overall_energy = 0
-        for i in range(cave_size):
-            for j in range(cave_size):
-                if (i, j) in already_flashed:
-                    # overall_energy += octo_cave[i][j]
+        for x in range(cave_size):
+            for y in range(cave_size):
+                if (x, y) in already_flashed:
                     continue
 
-                octo_cave[i][j] += 1
-                if octo_cave[i][j] > 9:
-                    already_flashed.add((i, j))
-                    octo_cave[i][j] = 0
-                    _propagate_energy(octo_cave, already_flashed, i, j)
-
-                # overall_energy += octo_cave[i][j]
+                _increment_octo_energy(x, y, octo_cave, already_flashed)
 
         if sum(sum(row) for row in octo_cave) == 0:
             return day + 1
 
     return -1
+
+
+def _increment_octo_energy(x: int, y: int, octo_cave: List[List[int]], already_flashed: Set[Tuple[int, int]]):
+    if (x, y) in already_flashed:
+        return 0
+
+    octo_cave[x][y] += 1
+
+    if octo_cave[x][y] > MAX_ADMISSIBLE_ENERGY:
+        octo_cave[x][y] = MIN_ADMISSIBLE_ENERGY
+        already_flashed.add((x, y))
+        return _propagate_energy(x, y, octo_cave, already_flashed) + 1
+
+    return 0
+
+
+def _propagate_energy(x: int, y: int, octo_cave: List[List[int]], already_flashed: Set[Tuple[int, int]]):
+    flashes_count, cave_size = 0, len(octo_cave) - 1
+    neighbours = [
+        (x + 1, y + 1), (x - 1, y - 1), (x + 1, y - 1),
+        (x - 1, y + 1), (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)
+    ]
+
+    for neighbour in neighbours:
+        xn, yn = neighbour
+
+        if 0 > xn or xn > cave_size or 0 > yn or yn > cave_size:
+            continue
+
+        flashes_count += _increment_octo_energy(xn, yn, octo_cave, already_flashed)
+
+    return flashes_count
 
 
 if __name__ == '__main__':
@@ -87,5 +76,6 @@ if __name__ == '__main__':
         for octo_row in file.readlines():
             octo_cave.append([int(energy_lvl) for energy_lvl in octo_row.strip()])
 
-    # print(count_octopuses_flashes(octo_cave, 100))
-    print(find_first_simultaneous_flash(octo_cave, 1000))
+    # we have to use deepcopy as we mutate our input ://
+    print(count_octopuses_flashes(deepcopy(octo_cave), 100))
+    print(find_first_simultaneous_flash(deepcopy(octo_cave), 1000))
