b12e7923b8587becbffe821d35a3413eb9223b54
mwoss <wos.mateusz16@gmail.com>
2022-01-06 00:08:29 +0100

    Refactor solution for day 12 a bit

--- day-12/solution.py
+++ day-12/solution.py
@@ -1,14 +1,14 @@
 from collections import defaultdict
-from typing import Dict, List, Set, Optional
+from typing import Dict, List, Set, Optional, Callable
 
 START_NODE_ID = "start"
 END_NODE_ID = "end"
 
 
-def count_unique_paths(cave: Dict[str, List[str]]) -> int:
+def count_unique_paths(cave: Dict[str, List[str]], path_counter: Callable) -> int:
     if START_NODE_ID not in cave or END_NODE_ID not in cave:
         raise Exception("One of require nodes (start/end) are missing in given cave representation")
-    return _count_unique_paths_single_entrance(START_NODE_ID, cave, {START_NODE_ID})
+    return path_counter(START_NODE_ID, cave, {START_NODE_ID})
 
 
 def _count_unique_paths_single_entrance(node: str, cave: Dict[str, List[str]], path: Set[str]) -> int:
@@ -23,22 +23,21 @@ def _count_unique_paths_single_entrance(node: str, cave: Dict[str, List[str]], p
     return count
 
 
-def count_unique_paths_2(cave: Dict[str, List[str]]) -> int:
-    if START_NODE_ID not in cave or END_NODE_ID not in cave:
-        raise Exception("One of require nodes (start/end) are missing in given cave representation")
-    return _count_unique_paths_2(START_NODE_ID, cave, {START_NODE_ID})
-
-
-def _count_unique_paths_2(node: str, cave: Dict[str, List[str]], path: Set[str], visited_twice: Optional[str] = None) -> int:
+def _count_unique_paths_twice_entrance(
+    node: str,
+    cave: Dict[str, List[str]],
+    path: Set[str],
+    visited_twice: Optional[str] = None
+) -> int:
     if node == END_NODE_ID:
         return 1
 
     count = 0
     for next_node in cave[node]:
         if next_node.isupper() or next_node not in path:
-            count += _count_unique_paths_2(next_node, cave, {*path, next_node}, visited_twice)
+            count += _count_unique_paths_twice_entrance(next_node, cave, {*path, next_node}, visited_twice)
         elif next_node != START_NODE_ID and visited_twice is None:
-            count += _count_unique_paths_2(next_node, cave, {*path, next_node}, next_node)
+            count += _count_unique_paths_twice_entrance(next_node, cave, {*path, next_node}, next_node)
 
     return count
 
@@ -51,5 +50,5 @@ if __name__ == '__main__':
             cave[start_node].append(end_node)
             cave[end_node].append(start_node)
 
-    print(count_unique_paths(cave))
-    print(count_unique_paths_2(cave))
+    print(count_unique_paths(cave, _count_unique_paths_single_entrance))
+    print(count_unique_paths(cave, _count_unique_paths_twice_entrance))
