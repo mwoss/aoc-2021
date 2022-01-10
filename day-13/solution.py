21156f313edd3c5735eef3ab2852b80f2df97fd4
mwoss <wos.mateusz16@gmail.com>
2022-01-10 01:58:49 +0100

    Add ultra massy solution for day 13

--- day-13/solution.py
+++ day-13/solution.py
@@ -1,3 +1,4 @@
+import pprint
 from dataclasses import dataclass
 from typing import List, Tuple, Set
 
@@ -11,36 +12,53 @@ class Fold:
 def fold_left(dots: Set[Tuple[int, int]], fold_pos: int, max_x: int) -> Set[Tuple[int, int]]:
     after_fold = set()
     for x, y in dots:
-        if x >= fold_pos:
-            after_fold.add((x-fold_pos, y))
-        else:
-            after_fold.add((abs(x-fold_pos+1), y))
+        len_after_fold = max(max_x - fold_pos, fold_pos)
+        after_fold.add((len_after_fold - abs(x - fold_pos), y))
     return after_fold
 
 
 def fold_up(dots: Set[Tuple[int, int]], fold_pos: int, max_y: int) -> Set[Tuple[int, int]]:
     after_fold = set()
+    len_after_fold = max(max_y - fold_pos, fold_pos)
     for x, y in dots:
-        if y <= fold_pos:
-            after_fold.add((x, y))
-        else:
-            after_fold.add((abs(x - fold_pos + 1), y))
+        new_y = len_after_fold - abs(y - fold_pos)
+        after_fold.add((x, new_y))
     return after_fold
 
 
 def count_dots_after_fold(dots: Set[Tuple[int, int]], folds: List[Fold]) -> int:
-    max_x = max(dots, key=lambda d: d[0])[0]
-    max_y = max(dots, key=lambda d: d[1])[1]
+
     #
     # board = [[0 for _ in range(max_x)] for _ in range(max_y)]
 
     for fold in folds:
+        max_x = max(dots, key=lambda d: d[0])[0]
+        max_y = max(dots, key=lambda d: d[1])[1]
+
         if fold.direction == "x":
             dots = fold_left(dots, fold.position, max_x)
         else:
             dots = fold_up(dots, fold.position, max_y)
 
+    print_dots(dots)
+
     return len(dots)
+
+
+def print_dots(dots):
+    max_x = max(dots, key=lambda d: d[0])[0]
+    max_y = max(dots, key=lambda d: d[1])[1]
+
+    board = [[0 for _ in range(max_x+1)] for _ in range(max_y+1)]
+
+    for x, y in dots:
+        board[y][x] = 1
+
+    print(board)
+    import matplotlib.pyplot as plt
+
+    plt.imshow(board)
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -58,4 +76,5 @@ if __name__ == '__main__':
                 direction, position = line.lstrip("fold along ").split("=")
                 folds.append(Fold(direction, int(position)))
 
-    print(count_dots_after_fold(dots, [folds[0]]))
+    # print(count_dots_after_fold(dots, [folds[0]]))
+    print(count_dots_after_fold(dots, folds))
