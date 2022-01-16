1f11cef269d616c662a383d705784074f0b19d45
mwoss <wos.mateusz16@gmail.com>
2022-01-16 23:45:06 +0100

    Add working solution for day 14, part 1

--- day-14/solution.py
+++ day-14/solution.py
@@ -1,17 +1,26 @@
+from collections import Counter
+
+
+def get_most_to_least_common_element_difference(polimer: str) -> int:
+    most_common_elements = Counter(polimer).most_common()
+    return most_common_elements[0][1] - most_common_elements[-1][1]
+
+
 def polimerize(template: str, insertions: dict, steps: int) -> str:
     polimers = list(template)
     for _ in range(steps):
         new_sequence = []
         for i in range(len(polimers) - 1):
             first, second = polimers[i], polimers[i + 1]
-            new_sequence.extend([first, insertions[f"{first}{second}"], second])
+            new_sequence.extend([first, insertions[f"{first}{second}"]])
+        new_sequence.append(polimers[-1])
         polimers = new_sequence
 
     return "".join(polimers)
 
 
 if __name__ == '__main__':
-    with open("input2.txt", "r") as file:
+    with open("input.txt", "r") as file:
         file_content = file.readlines()
 
     template = file_content[0].strip()
@@ -21,7 +30,5 @@ if __name__ == '__main__':
         match_pair, insert_polymer = insertion.strip().split(" -> ")
         pair_to_insertion[match_pair] = insert_polymer
 
-    print(template)
-    print(pair_to_insertion)
-
-    print(polimerize(template, pair_to_insertion, 2))
+    nth_polimer = polimerize(template, pair_to_insertion, 10)
+    print(get_most_to_least_common_element_difference(nth_polimer))
