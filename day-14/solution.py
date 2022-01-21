b1150e55f8c0de4e32560b2c846a3e0ae8b84e5c
mwoss <wos.mateusz16@gmail.com>
2022-01-21 01:50:45 +0100

    Refactor solution for day 14"

--- day-14/solution.py
+++ day-14/solution.py
@@ -1,47 +1,37 @@
-from collections import Counter, defaultdict
+from collections import defaultdict
+from typing import Dict
 
 
-def get_most_to_least_common_element_difference(polimer: str) -> int:
-    most_common_elements = Counter(polimer).most_common()
-    return most_common_elements[0][1] - most_common_elements[-1][1]
+def perform_insertion_process(polymer_template: str, insertions: Dict[str, str], steps: int) -> Dict[str, int]:
+    element_pairs_to_count = defaultdict(lambda: 0)
 
-
-def polimerize(template: str, insertions: dict, steps: int) -> str:
-    polimers = list(template)
-    for _ in range(steps):
-        new_sequence = []
-        for i in range(len(polimers) - 1):
-            first, second = polimers[i], polimers[i + 1]
-            new_sequence.extend([first, insertions[f"{first}{second}"]])
-        new_sequence.append(polimers[-1])
-        polimers = new_sequence
-
-    return "".join(polimers)
-
-
-def polimerize_cache(template: str, insertions: dict, steps: int) -> int:
-    element_map = defaultdict(lambda: 0)
-    letter_map = defaultdict(lambda: 0)
-    for i in range(len(template) - 1):
-        element_map[f'{template[i]}{template[i + 1]}'] += 1
+    for i in range(len(polymer_template) - 1):
+        element_pairs_to_count[f'{polymer_template[i]}{polymer_template[i + 1]}'] += 1
 
     for _ in range(steps):
         temp_element_map = defaultdict(lambda: 0)
-        temp_letter_map = defaultdict(lambda: 0)
-        for element, count in element_map.items():
-            letter = insertions[element]
-            temp_element_map[f'{element[0]}{letter}'] += count
-            temp_element_map[f'{letter}{element[1]}'] += count
-            temp_letter_map[element[0]] += count
-            temp_letter_map[element[1]] += count
-            temp_letter_map[letter] += count
+        for element_pair, count in element_pairs_to_count.items():
+            insert_element = insertions[element_pair]
+            temp_element_map[f'{element_pair[0]}{insert_element}'] += count
+            temp_element_map[f'{insert_element}{element_pair[1]}'] += count
+        element_pairs_to_count = temp_element_map
 
-        element_map = temp_element_map
-        letter_map = temp_letter_map
+    return element_pairs_to_count
 
-    most_common_element = max(letter_map.values())
-    least_common_element = min(letter_map.values())
-    return most_common_element - least_common_element
+
+def get_polymer_common_differance(element_pairs_to_count: Dict[str, int]) -> int:
+    element_counter = defaultdict(lambda: 0)
+    for element_pair, count in element_pairs_to_count.items():
+        element_counter[element_pair[0]] += count
+        element_counter[element_pair[1]] += count
+
+    most_common_element = max(element_counter.values())
+    least_common_element = min(element_counter.values())
+
+    # each letter was counted twice (besides polymers with odd number of elements) as we working of pair of elements
+    # which were created by moving windows of size 2, eg: NNCE -> [NN, NC, CE]
+    # we have to dived that number by 2, to get expected, correct answer
+    return (most_common_element - least_common_element + 1) // 2
 
 
 if __name__ == '__main__':
@@ -55,7 +45,8 @@ if __name__ == '__main__':
         match_pair, insert_polymer = insertion.strip().split(" -> ")
         pair_to_insertion[match_pair] = insert_polymer
 
-    nth_polimer = polimerize(template, pair_to_insertion, 10)
-    print(get_most_to_least_common_element_difference(nth_polimer))
+    init_elements_count = perform_insertion_process(template, pair_to_insertion, 10)
+    final_elements_count = perform_insertion_process(template, pair_to_insertion, 40)
 
-    print(polimerize_cache(template, pair_to_insertion, 10))
+    print(get_polymer_common_differance(init_elements_count))
+    print(get_polymer_common_differance(final_elements_count))
