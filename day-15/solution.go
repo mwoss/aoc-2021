673664f125657c36915d92cfcd35c9266c6a3aa6
Mateusz Woś <wos.mateusz16@gmail.com>
2022-02-05 23:35:12 +0100

    Potential fix for solution day 15 part 1

--- day-15/solution.go
+++ day-15/solution.go
@@ -11,6 +11,50 @@ import (
 	"strings"
 )
 
+// ............
+// An Item is something we manage in a priority queue.
+type Item struct {
+	value    int // The value of the item; arbitrary.
+	priority int // The priority of the item in the queue.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
+	return pq[i].priority > pq[j].priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+///...............
+
 func unpackInput(inp string) []int {
 	var levels []int
 	for _, level := range strings.Split(inp, "") {
@@ -84,7 +128,7 @@ func FindLowestRiskLevel() int {
 			edgeRow := edge / rowCount
 			edgeCol := edge - (edgeRow * rowCount)
 
-			newDist := dist[node.value] + riskLevels[edgeCol][edgeCol] // fix
+			newDist := dist[node.value] + riskLevels[edgeRow][edgeCol] // fix
 
 			if newDist < dist[edge] {
 				dist[edge] = newDist
@@ -93,6 +137,7 @@ func FindLowestRiskLevel() int {
 		}
 
 		if node.value == nodeCount-1 {
+			fmt.Println(dist[:10])
 			return dist[nodeCount-1]
 		}
 	}
