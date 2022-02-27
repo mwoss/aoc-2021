75a341a00900eeb16783be83cfaac08195d2d8af
mwoss <wos.mateusz16@gmail.com>
2022-02-27 23:46:26 +0100

    Add working solution for day 16, part 1 (vendorder from Akumatic)

--- day-16/solution.py
+++ day-16/solution.py
@@ -23,25 +23,25 @@ def sum_versions(payload: tuple) -> int:
         return payload[0]
     return payload[0] + sum(sum_versions(sub) for sub in payload[3])
 
-
 def decode_packet(packet: str) -> tuple:
+    # packet header
     version = int(packet[:3], 2)
-    type_id = int(packet[3:6], 2)
-    sub_packets = []
+    type_id = int(packet[3: 6], 2)
     size = 6
+    sub_packets = list()
 
-    if type_id == 4:
-        i = 6
-        while i < len(packet):
-            sub_packets.append(packet[i + 1:i + 5])
+    if type_id == 4: # literal value
+        for i in range(6, len(packet), 5):
+            sub_packets.append(packet[i+1: i+5])
             size += 5
             if packet[i] == "0":
                 break
+        sub_packets = int("".join(sub_packets), 2)
 
-    else:
+    else: # operator
         size += 1
-        if packet[6] == "0":
-            sub_size = int(packet[7:22], 2)
+        if packet[6] == "0": # length in bits of the subpacket
+            sub_size = int(packet[7: 22], 2)
             size += 15 + sub_size
             data = packet[22:]
             i = 0
@@ -49,7 +49,7 @@ def decode_packet(packet: str) -> tuple:
                 decoded_sub = decode_packet(data[i:])
                 sub_packets.append(decoded_sub)
                 i += decoded_sub[2]
-        else:  # packet[6] == 1, number of sub-packets
+        else: # packet[6] == 1, number of sub-packets
             sub_amount = int(packet[7: 18], 2)
             size += 11
             data = packet[size:]
@@ -58,8 +58,45 @@ def decode_packet(packet: str) -> tuple:
                 sub_packets.append(decoded_sub)
                 size += decoded_sub[2]
                 data = packet[size:]
-
+    
     return version, type_id, size, sub_packets
+
+# def decode_packet(packet: str) -> tuple:
+#     version = int(packet[:3], 2)
+#     type_id = int(packet[3:6], 2)
+#     sub_packets = []
+#     size = 6
+
+#     if type_id == 4:
+#         i = 6
+#         while i < len(packet):
+#             sub_packets.append(packet[i + 1:i + 5])
+#             size += 5
+#             if packet[i] == "0":
+#                 break
+
+#     else:
+#         size += 1
+#         if packet[6] == "0":
+#             sub_size = int(packet[7:22], 2)
+#             size += 15 + sub_size
+#             data = packet[22:]
+#             i = 0
+#             while i < sub_size:
+#                 decoded_sub = decode_packet(data[i:])
+#                 sub_packets.append(decoded_sub)
+#                 i += decoded_sub[2]
+#         else:  # packet[6] == 1, number of sub-packets
+#             sub_amount = int(packet[7: 18], 2)
+#             size += 11
+#             data = packet[size:]
+#             for i in range(sub_amount):
+#                 decoded_sub = decode_packet(data)
+#                 sub_packets.append(decoded_sub)
+#                 size += decoded_sub[2]
+#                 data = packet[size:]
+
+#     return version, type_id, size, sub_packets
 
 
 if __name__ == '__main__':
