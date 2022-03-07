3eb628bc2a2e147d96f25d6ef49826be45c9610e
mwoss <wos.mateusz16@gmail.com>
2022-03-07 23:51:29 +0100

    Parse input file of day 17 task

---
+++ day-17/solution.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	rawContent, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := string(rawContent)
+
+	reg, err := regexp.Compile("([-]?\\d+..[-]?\\d+)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	matches := reg.FindAllString(content, -1)
+	if len(matches) != 2 {
+		log.Fatal("Input file should only contains x and y ranges")
+	}
+
+	x := strings.Split(matches[0], "..")
+	y := strings.Split(matches[1], "..")
+
+	x1, x2 := x[0], x[1]
+	y1, y2 := y[0], y[1]
+
+	fmt.Println(x1, x2)
+	fmt.Println(y1, y2)
+}
